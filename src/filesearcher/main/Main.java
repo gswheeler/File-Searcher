35123b412f1d1ba333dea68a0431e447af7d862d
gswheeler <SilentRaptor22@gmail.com>
2015-11-15 15:40:16 -0500

    Initial sync: Main class and interface
    Created the NetBeans project. Created the Main class and the primary
    interface.

---
+++ src/filesearcher/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filesearcher.main;
+
+/**
+ *
+ * @author Greg
+ */
+public class Main {
+    
+    /**The function called when the program starts
+     * @param args Empty if the initial call for the interface. Has a single entry with the value "seeker" if it's a search call
+     */
+    public static void main(String[] args){
+        
+    }
+    
+}
