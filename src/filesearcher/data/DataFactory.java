154665bf9ea69bd28e03ee5639a86b0bb3c1bb4f
gswheeler <SilentRaptor22@gmail.com>
2015-11-17 20:27:55 -0500

    New class: data generation
    Added a new class to handle the generation of data (will hold the
    search-start call and the various search-file/folder functions).

---
+++ src/filesearcher/data/DataFactory.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filesearcher.data;
+
+/**
+ * Contains static functions regarding the generation of data.
+ */
+public class DataFactory {
+    
+    public static void runSearch(){}
+    
+}
